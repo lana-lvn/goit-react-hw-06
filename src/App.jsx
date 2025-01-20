1b4ecbf16035c984be3aea62c55cdf9f3302cca2
ruslana <ruslana.lohvyn@gmail.com>
2025-01-20 19:09:27 +0200

    refactored to slice and persist

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-
 import './App.css';
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import SearchBox from './components/SearchBox/SearchBox';
+
 
 function App() {
 
+
   return (
-    <>
-    </>
+    
+  <div className="wrapper">
+    <h1 className="title">Phonebook</h1>
+    <ContactForm />
+    <SearchBox />
+    <ContactList />
+  </div>
+  
   );
 };
 
